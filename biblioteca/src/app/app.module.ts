87203a6ada2ae1f4291edebcb50b6e10e497362e
Luana Silva <120854706+luafrds@users.noreply.github.com>
2024-06-17 22:10:20 -0300

    Corrige

--- biblioteca/src/app/app.module.ts
+++ biblioteca/src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     RouterModule,
+    FormsModule,
     CoreModule,
     NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
     ToastrModule.forRoot(),
     ModalModule,
-    BrowserAnimationsModule
+    NgSelectModule,
   ],
   providers: [BsModalService],
   bootstrap: [AppComponent]
